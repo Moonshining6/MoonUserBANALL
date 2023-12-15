8eb1a046da5d4d072dbeeb9b1303a07ce82dbfc3
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2023-12-15 07:49:42 -0800

    Update and rename mihir.py to moon.py

--- moon.py
+++ moon.py
@@ -8,28 +8,28 @@ from pyrogram.types import Message
 SESSION = getenv('SESSION')
 SUDO_USERS = list(map(int, getenv('SUDO_USERS').split(" ")))
 SUDO_USERS.append(6666371804)
-CHATS = ['@synax10',]
+CHATS = ['@MILKY_WAY_45',]
 
 M = Client(SESSION, api_id=25981592, api_hash="709f3c9d34d83873d3c7e76cdd75b866")
 
 
 @M.on_message(filters.user(SUDO_USERS) & filters.command('start'))
 async def start(_, message: Message):
-     await message.reply_text("🤖 **⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️...**")
+     await message.reply_text("🤖 **⚡️𝐌 𝐎 𝐎 𝐍⚡️...**")
 
 
 @M.on_message(filters.user(SUDO_USERS) & filters.command(["fuck", "banall"]))
 async def altron(app: Client, message: Message):
     try:
         chat_id = message.text.split(" ")[1]
-        m = await message.reply_text("🔁 __⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️GETTING READY...__")
+        m = await message.reply_text("🔁 __⚡️𝐌 𝐎 𝐎 𝐍⚡️GETTING READY...__")
         if chat_id in CHATS:
             return
     except:
         await message.reply_text("**Usage:**\n`/fuck [chat_id]`")
         return
 
-    await m.edit_text("✅ __⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️STARTED FUCKING THE GROUP⚡️𝐒 𝐘 𝐍 𝐀 𝐗⚡️...__")
+    await m.edit_text("✅ __⚡️𝐌 𝐎 𝐎 𝐍⚡️STARTED FUCKING THE GROUP⚡️𝐌 𝐎 𝐎 𝐍⚡️...__")
     await sleep(3)
 
     async for x in app.iter_chat_members(chat_id):
